bc7cbbd3517eb3bd9c9c4c16beac3c131909f758
Maira Bello <maira.bello@vtex.com.br>
2017-05-17 13:55:59 -0300

    Change apollo's `dataIdFromObject` config to only handle `id` fields specially
    We want this logic to be generic for all masterdata schemas, so this is removing the specific id logic for `slug` and other fields, and keeping only the pattern of having `${__typename}:${id}`. To have data automatically cached in the client, users will need to provide an `id` field for their types.

--- render/apolloConfig.js
+++ render/apolloConfig.js
@@ -6,13 +6,6 @@ export const addCustomResolvers = (resolvers) => {
 
 export const getCustomResolvers = () => customResolvers
 
-export function getDataIdFromObject (result) {
-  const id = (result.slug || result.orderFormId)
-  if (result.__typename === 'Facet') {
-    return null
-  }
-  if (id && result.__typename) {
-    return result.__typename + ':' + id
-  }
-  return null
+export function getDataIdFromObject ({id, __typename}) {
+  return id && __typename ? `${__typename}:${id}` : null
 }
