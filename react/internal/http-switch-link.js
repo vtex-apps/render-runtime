97c3b1e94a15d30d03d9564d0456b3dd0f6438be
Tiago Gimenes <tlgimenes@gmail.com>
2018-01-25 19:12:37 -0200

    added GET for Query types and POST for mutation types in GraphQL

---
+++ react/internal/http-switch-link.js
@@ -0,0 +1,58 @@
+/*
+ * This file defines a linker middleware used for changing http verbs, allowing
+ * cache in CDNs and other http-based caching content delivery networks
+ *
+ * If the query type is Query, the request will be made using an HTTP GET
+ * If the query type is Mutation, the request will be made using HTTP POST
+ *
+ */
+import {ApolloLink} from 'apollo-link'
+import {parse, format} from 'url'
+import {print} from 'graphql'
+
+const isEmpty = (val) => (val == undefined || Object.keys(val).length === 0)
+
+const hasMutationField = (queryTree) => {
+  for (let query of queryTree.definitions) {
+    if (query.operation === 'mutation') {
+      return true
+    }
+  }
+  return false
+}
+
+export const createHttpSwitchLink = (uri) => {
+  const parsedUri = parse(uri, true)
+  // Delete search in order to format to use query, instead of search, field while
+  // formatting the new URI
+  delete parsedUri.search
+
+  return new ApolloLink((operation, forward) => {
+    const targetUri = Object.assign({}, parsedUri)
+    const {query, variables, operationName} = operation
+    const {fetchOptions = {}} = operation.getContext()
+    const method = hasMutationField(query) ? 'POST' : 'GET'
+
+    if(method === 'GET') {
+      fetchOptions['method'] = method
+
+      targetUri.query = {
+        ...targetUri.query,
+        'query': print(query).replace(/\s\s+/g, ' '),
+        variables,
+        operationName
+      }
+    }
+
+    operation.setContext({uri: format(targetUri), fetchOptions})
+    return forward(operation)
+  })
+}
+
+export const httpSwitchFetcher = (uri, options) => {
+  if(options.method === 'GET' && options.body) {
+    delete options.body
+  }
+
+  return fetch(uri, options)
+}
