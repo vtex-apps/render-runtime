9f5497a75ee54833e0f348ed7341bdc5f6d22a1f
Guilherme Rodrigues <gadr90@gmail.com>
2018-01-28 13:07:06 -0200

    Fix regex

--- react/start.js
+++ react/start.js
@@ -8,7 +8,7 @@ if (global.__RUNTIME__.start) {
 
 if (module.hot) {
   if (canUseDOM) {
-    const runtimeIndexComponent = Object.keys(global.__RENDER_6_COMPONENTS__).find((c) => /vtex\.render-runtime@6\.0\.4\/index/.test(c))
+    const runtimeIndexComponent = Object.keys(global.__RENDER_6_COMPONENTS__).find((c) => /vtex\.render-runtime@.*\/index/.test(c))
     global.__RUNTIME__.eventEmitter.addListener(`component:${runtimeIndexComponent}:update`, startRuntime)
   }
 }
