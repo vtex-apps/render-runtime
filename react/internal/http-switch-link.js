54188d7ca61efa194e128de588f01d8617c3af31
Tiago Gimenes <tlgimenes@gmail.com>
2018-01-30 18:25:58 -0200

    bugfix in multiple querystring generation

--- react/internal/http-switch-link.js
+++ react/internal/http-switch-link.js
@@ -6,14 +6,10 @@
  * If the query type is Mutation, the request will be made using HTTP POST
  *
  */
-///////////////////////////////////////////////////////////////////////////////
-
 import {ApolloLink} from 'apollo-link'
 import { prop, map, contains, isEmpty, isNil } from 'ramda'
 import { parse, stringify } from 'querystring'
 import { print } from 'graphql'
-
-///////////////////////////////////////////////////////////////////////////////
 
 function removeUnusedFields(obj) {
   Object.keys(obj).forEach(key => (isNil(obj[key]) || isEmpty(obj[key])) && delete obj[key])
@@ -21,44 +17,38 @@ function removeUnusedFields(obj) {
   return obj
 }
 
-///////////////////////////////////////////////////////////////////////////////
+export const createHttpSwitchLink = (uri) => {
+  const [baseUrl, querystring] = uri.split('?')
+  let queryObj = (querystring) ? parse(querystring) : {}
 
-export const createHttpSwitchLink = (uri) => new ApolloLink((operation, forward) => {
-  const {query, variables, operationName} = operation
-  const {fetchOptions = {}, http: httpOptions = {}} = operation.getContext()
-  const method = contains('mutation',map(prop('operation'), prop('definitions')(query))) ?
-    'POST' :
-    'GET'
+  return new ApolloLink((operation, forward) => {
+    let targetUri = uri
+    const {query, variables, operationName} = operation
+    const {fetchOptions = {}, http: httpOptions = {}} = operation.getContext()
+    const method = contains('mutation',map(prop('operation'), prop('definitions')(query))) ?
+      'POST' :
+      'GET'
 
-  // If method is GET, we need to set the querystring correctly
-  if(method === 'GET') {
-    fetchOptions['method'] = method
+    if(method === 'GET') {
+      fetchOptions['method'] = method
 
-    // Retrieves base URL and querystring from URL
-    const [baseUrl, querystring] = uri.split('?')
+      const fullQueryObj = removeUnusedFields({
+        ...queryObj,
+        'query': print(query).replace(/\s\s+/g, ' '),
+        variables,
+        operationName
+      })
 
-    // Transforms it back into a JSON
-    let queryObj = (querystring) ? parse(querystring) : {}
+      targetUri = baseUrl.concat(`?${stringify(fullQueryObj)}`)
+    }
 
-    // Builds a new query object to be send as querystring
-    queryObj = removeUnusedFields({
-      ...queryObj,
-      'query': print(query).replace(/\s\s+/g, ' '),
-      variables,
-      operationName
-    })
+    console.log(targetUri)
 
-    // Stringify the JSON and set it as the new querystring for the target URL
-    uri = baseUrl.concat(`?${stringify(queryObj)}`)
-  }
+    operation.setContext({uri: targetUri, fetchOptions})
+    return forward(operation)
+  })
+}
 
-  // Sets the new URI and fetchOptions to the operation context and return
-  operation.setContext({uri, fetchOptions})
-  return forward(operation)
-})
-
-///////////////////////////////////////////////////////////////////////////////
-// Custom fetcher to remove body in GET requests
 export const httpSwitchFetcher = (uri, options) => {
   if(options.method === 'GET' && options.body) {
     delete options.body
@@ -66,5 +56,3 @@ export const httpSwitchFetcher = (uri, options) => {
 
   return fetch(uri, options)
 }
-
-///////////////////////////////////////////////////////////////////////////////
