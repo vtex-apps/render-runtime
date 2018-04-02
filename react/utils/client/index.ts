5f2c650b44eeaa3ad5f6486d0b63639c0a7e7057
Bruno Abreu <bruno.abreu@vtex.com.br>
2018-04-02 18:17:07 -0300

    Remove unused dependency

--- react/utils/client/index.ts
+++ react/utils/client/index.ts
@@ -1,7 +1,6 @@
 import {InMemoryCache, NormalizedCacheObject} from 'apollo-cache-inmemory'
 import {ApolloClient} from 'apollo-client'
 import {ApolloLink} from 'apollo-link'
-import {BatchHttpLink} from 'apollo-link-batch-http'
 import {createHttpLink} from 'apollo-link-http'
 import {createPersistedQueryLink} from 'apollo-link-persisted-queries'
 import {canUseDOM} from 'exenv'
