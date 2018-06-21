755917161663edbe2123dd15a5b2e8de7c69d31c
Bruno Abreu <bruno.abreu@vtex.com.br>
2018-06-21 17:00:00 -0300

    Fix sorting direct children with numeric values
    This enables sorting numeric treePaths or ending with numbers properly
    Ex: ['b20', 'a0', 'a10', 'a1', '11', 'b2', '2', '1', '10'] becomes ['1', '2', '10', '11', 'a0', 'a1', 'a10', 'a11', 'b2']

--- react/utils/treePath.tsx
+++ react/utils/treePath.tsx
@@ -8,11 +8,25 @@ const isDirectChild = (id: string, parent: string) => {
   return id !== parent && (new RegExp(`^${parent}/[a-zA-Z0-9-]+$`)).test(id)
 }
 
+const parseId = (id: string) => {
+  const [, text, numericText] = /^(.*?)(\d+)?$/.exec(id)!
+  const numbericValue = numericText ? parseInt(numericText, 10) : 0
+  return [text, numbericValue]
+}
+
 export const getDirectChildren = (extensions: Extensions, treePath: string) => {
   return Object.entries(extensions)
     .filter(([id, extension]) => extension.component && isDirectChild(id, treePath))
     .map(([id]) => relative(treePath, id))
-    .sort()
+    .sort((idA, idB) => {
+      const [textA, numberA] = parseId(idA)
+      const [textB, numberB] = parseId(idB)
+      const [valueA, valueB] = (textA === textB)
+        ? [numberA, numberB]
+        : [textA, textB]
+
+      return valueA < valueB ? -1 : 1
+    })
 }
 
 export const TreePathContext = React.createContext<TreePathProps>({treePath: ''})
