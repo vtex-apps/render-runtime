d15dfdd002734a872e40aa9a522ef99a55306b47
Guilherme Rodrigues <gadr90@gmail.com>
2018-07-07 19:57:20 -0300

    Do not pass runtime to withHMR

--- react/components/RenderContext.tsx
+++ react/components/RenderContext.tsx
@@ -5,9 +5,18 @@ export interface RenderContextProps {
   runtime: RenderContext
 }
 
+export interface EmitterProps {
+  __emitter: RenderContext['emitter']
+}
+
 export const RenderContext = React.createContext<RenderContext>({} as any)
 
 export const withContext = <TOriginalProps extends {} = {}>(Component: ComponentType<TOriginalProps & RenderContextProps>): ComponentType<TOriginalProps> => {
   const ExtendedComponent = (props: TOriginalProps) => <RenderContext.Consumer>{runtime => <Component {...props} runtime={runtime} />}</RenderContext.Consumer>
   return hoistNonReactStatics<TOriginalProps, RenderContextProps>(ExtendedComponent, Component)
+}
+
+export const withEmitter = <TOriginalProps extends {} = {}>(Component: ComponentType<TOriginalProps & EmitterProps>): ComponentType<TOriginalProps> => {
+  const ExtendedComponent = (props: TOriginalProps) => <RenderContext.Consumer>{runtime => <Component {...props} __emitter={runtime.emitter} />}</RenderContext.Consumer>
+  return hoistNonReactStatics<TOriginalProps, EmitterProps>(ExtendedComponent, Component)
 }
