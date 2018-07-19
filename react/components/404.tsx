d1151c3c974087c73e8892fa22124ef01c077ed7
Lucas Cordeiro <ecdb.lucas@gmail.com>
2018-07-19 14:08:41 -0300

    Fix MaybeAuth not updating on page change

---
+++ react/components/404.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export default class NotFound extends React.Component {
+  public render() {
+    return (
+      <h2>
+        404 - The page you request was not found.
+      </h2>
+    )
+  }
+}
