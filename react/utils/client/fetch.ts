16523d5bdfcddf29356d87a9e468e6fa62e3b062
Tiago Gimenes <tlgimenes@gmail.com>
2018-09-10 18:30:38 -0300

    Makes compliant to url query array

--- react/utils/client/fetch.ts
+++ react/utils/client/fetch.ts
@@ -1,19 +1,23 @@
 import {format, parse} from 'url'
+import URLSearchParams from 'url-search-params'
+
+const separator = '@@'
+
+const graphqlStringify = (obj: any) => [obj.operationName, JSON.stringify(obj.variables || {}), JSON.stringify(obj.extensions || {})].join(separator)
 
 export const IOFetch = async (urlOrRequest: string | Request, init?: RequestInit): Promise<Response> => {
   const {useGetInBatch = false, body} = init || {} as any
   const formattedInit: RequestInit = {...init}
-  let formattedUrl = (typeof urlOrRequest === 'string') ? urlOrRequest : urlOrRequest.url
+  let formattedUrl = typeof urlOrRequest === 'string' ? urlOrRequest : urlOrRequest.url
 
   if (useGetInBatch && formattedInit) {
     const parsedUri = parse(formattedUrl, true)
-
-    delete parsedUri.search
-    parsedUri.query = {...parsedUri.query, batch: body}
-
-    delete formattedInit.body
+    const formattedBody = JSON.parse(body || '{}')
+    const searchParams = new URLSearchParams(parsedUri.search)
+    formattedBody.forEach((obj: any) => searchParams.append('batch', graphqlStringify(obj)))
+    parsedUri.search = searchParams.toString()
+    formattedInit.body = undefined
     formattedInit.method = 'GET'
-
     formattedUrl = format(parsedUri)
   }
 
