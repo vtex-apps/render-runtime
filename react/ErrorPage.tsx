2f848914cda164086cf0209f0519efa0292cdf60
Guilherme Rodrigues <gadr90@gmail.com>
2018-09-30 20:43:47 -0300

    Add splunk link

--- react/ErrorPage.tsx
+++ react/ErrorPage.tsx
@@ -1,9 +1,15 @@
 import React, {Component} from 'react'
 
+const toSplunkLink = (rid: string) =>
+  `https://splunk7.vtex.com/en-US/app/vtex_colossus/search?q=search%20index%3Dcolossus%20sender%3Dvtex.render-server%40*%20body.requestId%3D${rid}&display.page.search.mode=verbose&dispatch.sample_ratio=1&earliest=-5m%40m&latest=now`
+
 export default class ErrorPage extends Component {
   public render() {
     return (
-      <div>Error: {JSON.stringify(__RUNTIME__.error)}</div>
+      <div>
+        Error: {__REQUEST_ID__}
+        <a href={toSplunkLink(__REQUEST_ID__)} target="_blank">Search for this error in Splunk</a>
+      </div>
     )
   }
 }
