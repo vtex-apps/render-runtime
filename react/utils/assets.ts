c26985b74c1b0614eadf9fbead052cd2372f8dc0
Guilherme Rodrigues <gadr90@gmail.com>
2018-12-02 12:28:16 -0200

    Preload assets on prefetch instead of evaluating

--- react/utils/assets.ts
+++ react/utils/assets.ts
@@ -17,8 +17,17 @@ const getAbsoluteURL = (account: string, url: string) => {
     : url
 }
 
+class ServerSideAssetLoadingError extends Error {
+  constructor() {
+    super('Loading assets on server side rendering is not supported')
+  }
+}
+
 export function addScriptToPage(src: string): Promise<void> {
   return new Promise((resolve, reject) => {
+    if (!document || !document.head) {
+      throw new ServerSideAssetLoadingError()
+    }
     const script = document.createElement('script')
     script.onload = () => resolve()
     script.onerror = () => reject()
@@ -29,6 +38,9 @@ export function addScriptToPage(src: string): Promise<void> {
 }
 
 function addStyleToPage(href: string) {
+  if (!document || !document.head) {
+    throw new ServerSideAssetLoadingError()
+  }
   const link = document.createElement('link')
   link.href = href
   link.type = 'text/css'
@@ -36,10 +48,32 @@ function addStyleToPage(href: string) {
   document.head.appendChild(link)
 }
 
+function preloadStyle(href: string) {
+  if (!document || !document.head) {
+    throw new ServerSideAssetLoadingError()
+  }
+  const link = document.createElement('link')
+  link.href = href
+  link.as = 'style'
+  link.rel = 'preload'
+  document.head.appendChild(link)
+}
+
+function preloadScript(href: string) {
+  if (!document || !document.head) {
+    throw new ServerSideAssetLoadingError()
+  }
+  const link = document.createElement('link')
+  link.href = href
+  link.as = 'script'
+  link.rel = 'preload'
+  document.head.appendChild(link)
+}
+
 function getExistingScriptSrcs() {
   const paths: string[] = []
   for (let i = 0; i < document.scripts.length; i++) {
-    paths.push(document.scripts.item(i).src)
+    paths.push(document.scripts.item(i)!.src)
   }
   return paths
 }
@@ -55,12 +89,27 @@ function getExistingStyleHrefs() {
   return hrefs
 }
 
+function getExistingPreloadLinks() {
+  const paths: string[] = []
+  for (let i = 0; i < document.links.length; i++) {
+    const item = document.links.item(i)
+    if (item && item.rel === 'preload') {
+      paths.push(item.href)
+    }
+  }
+  return paths
+}
+
 function scriptOnPage(path: string) {
   return getExistingScriptSrcs().some(src => src.indexOf(path) !== -1)
 }
 
 function styleOnPage(path: string) {
   return getExistingStyleHrefs().some(href => href.indexOf(path) !== -1)
+}
+
+function assetPreloaded(path: string) {
+  return getExistingPreloadLinks().some(href => href.indexOf(path) !== -1)
 }
 
 function isScript(path: string) {
@@ -79,6 +128,14 @@ function shouldAddStyleToPage(path: string) {
   return isStyle(path) && !styleOnPage(path)
 }
 
+function shouldPreloadScript(path: string) {
+  return isScript(path) && !scriptOnPage(path) && !assetPreloaded(path)
+}
+
+function shouldPreloadStyle(path: string) {
+  return isStyle(path) && !styleOnPage(path) && !assetPreloaded(path)
+}
+
 export function getImplementation<P={}, S={}>(component: string) {
   return window.__RENDER_7_COMPONENTS__[component] as RenderComponent<P, S>
 }
@@ -94,4 +151,11 @@ export function fetchAssets(account: string, assets: string[]) {
   const styles = absoluteAssets.filter(shouldAddStyleToPage)
   styles.forEach(addStyleToPage)
   return Promise.all(scripts.map(addScriptToPage)).then(() => { return })
+}
+
+export function preloadAssets(assets: string[]) {
+  const scripts = assets.filter(shouldPreloadScript)
+  const styles = assets.filter(shouldPreloadStyle)
+  scripts.forEach(preloadScript)
+  styles.forEach(preloadStyle)
 }
