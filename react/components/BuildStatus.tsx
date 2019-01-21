30ad5c88e4d4e1b10799696f0cda31eeb82604c9
Lucas Bebber <lucasbbebber@gmail.com>
2019-01-21 14:10:33 -0200

    Reduce opacity of the background of the load indicator a bit

--- react/components/BuildStatus.tsx
+++ react/components/BuildStatus.tsx
@@ -11,7 +11,7 @@ interface State {
 
 const buildStatusLoading = (
   <svg width="26px" height="26px" viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
-    <circle cx="50" opacity="0.4" cy="50" fill="none" stroke="#F71963" strokeWidth="14" r="40"></circle>
+    <circle cx="50" opacity="0.2" cy="50" fill="none" stroke="#F71963" strokeWidth="14" r="40"></circle>
     <circle cx="50" cy="50" fill="none" stroke="#F71963" strokeWidth="12" r="40" strokeDasharray="60 900" strokeLinecap="round" transform="rotate(96 50 50)">
       <animateTransform attributeName="transform" type="rotate" calcMode="linear" values="0 50 50;360 50 50" keyTimes="0;1" dur="0.7s" begin="0s" repeatCount="indefinite"></animateTransform>
     </circle>
