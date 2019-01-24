efed814904e741ceb36b95c63e3f47f7164796c1
Luciano Júnior <luciannojunior@gmail.com>
2019-01-24 13:44:39 -0200

    Removing logging artifacts

--- react/utils/linkState.ts
+++ react/utils/linkState.ts
@@ -27,16 +27,12 @@ const mergeReducer = (
 
 const GLOBAL_MAP = window.__RENDER_8_COMPONENTS__
 
-const peak = msg => i => {console.log(msg, ' =>>', i); return i}
-
 export const getGlobalLinkState = () =>
   pipe(
       keys,
       filter(endsWith('LinkState')),
       uniqBy(id => head(split('@', id))),
       map(flip(prop)(GLOBAL_MAP)),
-      peak('Depois do filter'),
       reduce(mergeReducer, {initialState: {}, resolvers: { Mutation: {} }}),
-      peak('Depois do reduce'),
 
   )(GLOBAL_MAP)
