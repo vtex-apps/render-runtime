9263185e90a3b6705613632a8a0f061d12d6f18b
tiagonapoli <napoli.tiago96@gmail.com>
2019-02-18 15:44:15 -0300

    Remove reactCreatePortal rename

--- react/components/ExtensionPortal.tsx
+++ react/components/ExtensionPortal.tsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types'
 import React, { Component } from 'react'
-import {createPortal as reactCreatePortal} from 'react-dom'
+import { createPortal } from 'react-dom'
 
 import { PortalRenderingRequest } from './ExtensionManager'
 import ExtensionPoint from './ExtensionPoint'
@@ -21,7 +21,7 @@ class ExtensionPortal extends Component<Props> {
 
   public render() {
     const { extensionName, destination, props } = this.props.extension
-    return reactCreatePortal(<ExtensionPoint id={extensionName} {...props} />, destination)
+    return createPortal(<ExtensionPoint id={extensionName} {...props} />, destination)
   }
 }
 
