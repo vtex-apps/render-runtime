343300134e5c1057c924070dfb2a3e607d2d6d86
Tiago Gimenes <tlgimenes@gmail.com>
2019-03-12 01:53:06 -0300

    using onpage changed to refetch messages and other data that may be changed

--- react/utils/messages.ts
+++ react/utils/messages.ts
@@ -5,10 +5,3 @@ const YEAR_IN_MS = 12 * 30 * 24 * 60 * 60 * 1000
 export const parseMessages = (messages: KeyedString[] | null) => {
   return messages && zipObj(pluck('key', messages), pluck('message', messages))
 }
-
-export const createLocaleCookie = (locale: string) => {
-  const yearFromNow = Date.now() + YEAR_IN_MS
-  const expires = new Date(yearFromNow).toUTCString()
-  const localeCookie = `locale=${locale};path=/;expires=${expires}`
-  window.document.cookie = localeCookie
-}
