387a036d3ba643352833efefc9d1dc6c6ff942fa
Lucas Bebber <lucasbbebber@gmail.com>
2019-03-19 20:02:45 -0300

    Remove loading timeout

--- react/components/Loading.tsx
+++ react/components/Loading.tsx
@@ -9,42 +9,9 @@ interface Props {
   preview?: PreviewProps
 }
 
-interface State {
-  visible?: boolean
-}
-
-const LOADING_TRESHOLD_MS = 1000
-// If a Loading component unmounts and remounts within this time, it should be visible from the start.
-const LOADING_UNMOUNT_TRESHOLD_MS = 1000
-
-class Loading extends Component<RenderContextProps & Props, State> {
-  public state: State = {
-    visible: false,
-  }
-  private thresholdTimeout?: number
-
-  public componentDidMount() {
-    if (!this.state.visible) {
-      this.thresholdTimeout = window.setTimeout(() => {
-        this.setState({ visible: true })
-      }, LOADING_TRESHOLD_MS)
-    }
-  }
-
-  public componentWillUnmount() {
-    if (this.thresholdTimeout) {
-      clearTimeout(this.thresholdTimeout)
-    }
-
-    window.setTimeout(() => {
-      this.setState({ visible: false })
-    }, LOADING_UNMOUNT_TRESHOLD_MS)
-  }
-
+class Loading extends Component<RenderContextProps & Props> {
   public render() { // tslint:disable-line member-ordering
     const { runtime: { extensions } } = this.props
-    const { visible: isVisible } = this.state
-    const style: CSSProperties = { visibility: 'hidden' }
 
     return (
       <TreePathContext.Consumer>
@@ -53,13 +20,7 @@ class Loading extends Component<RenderContextProps & Props, State> {
 
           const preview = (t && extensions[t] && extensions[t].preview) || this.props.preview
 
-          const loadingComponent = preview && <Preview {...preview} />
-
-          return (
-            <div style={isVisible ? undefined : style}>
-              {loadingComponent}
-            </div>
-          )
+          return preview && <Preview {...preview} />
         }}
       </TreePathContext.Consumer>
     )
