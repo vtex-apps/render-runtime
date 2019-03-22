a2f9b23d4c120123179a7590e2ec5896a5a77a40
Lucas Bebber <lucasbbebber@gmail.com>
2019-03-22 02:02:54 -0300

    Throw error in case of invalid props

--- react/components/ChildBlock.tsx
+++ react/components/ChildBlock.tsx
@@ -2,7 +2,17 @@ import React from 'react'
 import { useTreePath } from '../utils/treePath'
 import { useRuntime } from './RenderContext'
 
-export function useChildBlock({ id }: ChildBlock) : Block | null {
+export function useChildBlock(childBlock: ChildBlock) : Block | null {
+  if (typeof childBlock === 'string') {
+    throw new Error(`You are passing a string as a parameter to useChildBlock ("${childBlock}"). You should pass an object like {id: "${childBlock}"}.`)
+  }
+
+  const { id } = childBlock
+
+  if (!id) {
+    throw new Error('The id you are sending to useChildBlock is empty')
+  }
+
   const runtime = useRuntime()
   const { treePath } = useTreePath()
 
