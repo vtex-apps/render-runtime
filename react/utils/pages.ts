21db8f6edd6d074f4942667a76d4863b9c1e5000
Lucas Bebber <lucasbbebber@gmail.com>
2019-07-22 18:57:11 -0300

    Get template from first parameter of map query string

--- react/utils/pages.ts
+++ react/utils/pages.ts
@@ -1,7 +1,7 @@
 import { canUseDOM } from 'exenv'
 import { History, LocationDescriptorObject } from 'history'
 import queryString from 'query-string'
-import { difference, includes, is, isEmpty, keys } from 'ramda'
+import { difference, is, isEmpty, keys, startsWith } from 'ramda'
 import RouteParser from 'route-parser'
 
 const EMPTY_OBJECT = (Object.freeze && Object.freeze({})) || {}
@@ -332,11 +332,7 @@ function routeMatchForMappedURL(
 
   for (const name in routes) {
     const { map = [], path: routePath } = routes[name]
-    if (
-      !routePath ||
-      map.length === 0 ||
-      !includes(map.join(','), mappedSegments.join(','))
-    ) {
+    if (!routePath || map.length === 0 || !startsWith(map, mappedSegments)) {
       continue
     }
 
