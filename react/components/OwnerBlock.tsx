abc3f3edee9912e0c82adb1d17856b983057f501
Lucas Bebber <lucasbbebber@gmail.com>
2019-08-06 04:28:29 -0300

    Add props to OwnerBlock

--- react/components/OwnerBlock.tsx
+++ react/components/OwnerBlock.tsx
@@ -7,43 +7,58 @@ interface OwnerBlockValue {
   name: string
   vendor: string
   version: string
+  props: Record<string, any>
 }
 
-const OwnerBlockContext = React.createContext<OwnerBlockValue | undefined>(undefined)
+type EmptyOwnerBlockValue = Record<keyof OwnerBlockValue, undefined>
+
+const initialValue: EmptyOwnerBlockValue = {
+  identifier: undefined,
+  name: undefined,
+  vendor: undefined,
+  version: undefined,
+  props: undefined,
+}
+
+const OwnerBlockContext = React.createContext<
+  OwnerBlockValue | EmptyOwnerBlockValue
+>({
+  ...initialValue,
+})
 
 /**
  * This is adds a context of which block the current tree is owned by.
  */
-export const OwnerBlockProvider: FunctionComponent = (props) => {
+export const OwnerBlockProvider: FunctionComponent = props => {
   const runtime = useRuntime()
   const { treePath } = useTreePath()
 
   const extension = runtime.extensions[treePath]
-
-  if (!extension || !extension.blockId) {
-    return (
-      <OwnerBlockContext.Provider value={undefined} />
-    )
-  }
-
-  const { blockId } = extension
+  const blockProps = extension && extension.props
+  const blockId = extension && extension.blockId
 
   const value = useMemo(() => {
+    if (!blockId) {
+      return { ...initialValue }
+    }
+
     const vendor = blockId.slice(0, blockId.indexOf('.'))
     const name = blockId.slice(blockId.indexOf('.') + 1, blockId.indexOf('@'))
-    const version = blockId.slice(blockId.indexOf('@') + 1, blockId.indexOf(':'))
+    const version = blockId.slice(
+      blockId.indexOf('@') + 1,
+      blockId.indexOf(':')
+    )
 
     return {
       identifier: `${vendor}.${name}@${version}`,
       name,
       vendor,
       version,
+      props: blockProps,
     }
-  }, [blockId])
+  }, [blockId, blockProps])
 
-  return (
-    <OwnerBlockContext.Provider value={value} {...props} />
-  )
+  return <OwnerBlockContext.Provider value={value} {...props} />
 }
 
 export const useOwnerBlock = () => {
@@ -55,4 +70,3 @@ export const useOwnerBlock = () => {
 
   return context
 }
-
