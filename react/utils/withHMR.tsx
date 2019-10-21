d933451587b379d8810cba7d51b728cf5a9a7eda
Lucas Bebber <lucasbbebber@gmail.com>
2019-10-21 17:41:47 -0300

    Wording on withHMR update

--- react/utils/withHMR.tsx
+++ react/utils/withHMR.tsx
@@ -61,7 +61,7 @@ export default (module: Module, InitialImplementer: any) => {
       }
 
       console.log(
-        `[render] Component updated. treePath=${treePath} updated=${HMRComponent.displayName}`
+        `[render] Component updated via HMR. treePath=${treePath} updated=${HMRComponent.displayName}`
       )
     }
 
