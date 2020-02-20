6bf8eff96bd285eaaa5f1d3eee3a1f89c96e6ffa
Lucas Bebber <lucasbbebber@gmail.com>
2020-02-20 10:10:28 -0300

    Rename ExtensionPointFunctionComponent to ComponentLoader, add SiteEditorWrapper to add missing functionality from ExtensionPointClassComponent, and update types

--- react/components/MaybeContext.tsx
+++ react/components/MaybeContext.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo, Fragment, FC } from 'react'
-import ExtensionPointComponent from '../components/ExtensionPointClassComponent'
+import ComponentLoader from '../components/ExtensionPoint/ComponentLoader'
 import { RenderContextProps } from './RenderContext'
 import { ExtensionPoint } from '../core/main'
 
@@ -77,14 +77,14 @@ const MaybeContext: FC<Props> = ({
   })
 
   return contextComponent ? (
-    <ExtensionPointComponent
+    <ComponentLoader
       component={contextComponent}
       props={contextProps}
       runtime={runtime}
       treePath={nestedPage}
     >
       {children}
-    </ExtensionPointComponent>
+    </ComponentLoader>
   ) : (
     <Fragment>{children}</Fragment>
   )
