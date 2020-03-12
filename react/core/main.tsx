3b199fa9d9dc316bb4b7d3264319c3c194330c20
Lucas Bebber <lucasbbebber@gmail.com>
2020-03-12 14:52:32 -0300

    Remove commented out lazy img loading

--- react/core/main.tsx
+++ react/core/main.tsx
@@ -245,7 +245,6 @@ function start() {
 
       if (type === 'img') {
         props.src = optimizeSrcForVtexImg(vtexImgHost, props.src)
-        // props.loading = 'lazy'
         if (
           typeof props.src === 'string' &&
           props.src.startsWith(vtexImgHost)
