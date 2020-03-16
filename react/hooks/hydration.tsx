698502acac00b334303da3321b4d9d1f7ec56426
Lucas Bebber <lucasbbebber@gmail.com>
2020-03-16 20:04:08 -0300

    Memoize dehydratedElement and add comment

--- react/hooks/hydration.tsx
+++ react/hooks/hydration.tsx
@@ -1,16 +1,24 @@
 import { useRef } from 'react'
 
+/** Gets HTML content from elements rendered on the server, before being
+ * hydrated on the client (and whether they have been SSRendered at all). */
 export const useDehydratedContent = (hydrationId: string) => {
-  const dehydratedElement = window?.document?.querySelector?.(
-    `[data-hydration-id="${hydrationId}"]`
+  /** On both cases below, the use of useRef is to guarantee that only the values
+   * that it has been initialized with is being considered, to guarantee that it's
+   * only taking into account the HTML that's coming from the server, instead of
+   * eventual elements that might be included later.
+   */
+  const dehydratedElement = useRef(
+    window?.document?.querySelector?.(`[data-hydration-id="${hydrationId}"]`)
   )
 
   const hasDehydratedContent = useRef(
-    !!dehydratedElement && dehydratedElement.childElementCount > 0
+    !!dehydratedElement.current &&
+      dehydratedElement.current.childElementCount > 0
   )
 
-  const hasRenderedOnServer =
-    !!dehydratedElement && hasDehydratedContent.current
-
-  return { hasRenderedOnServer, dehydratedElement }
+  return {
+    hasRenderedOnServer: hasDehydratedContent.current,
+    dehydratedElement: dehydratedElement.current,
+  }
 }
