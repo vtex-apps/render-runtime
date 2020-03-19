50233900f3f1a376a7fb1f90695432c09982f56f
Lucas Bebber <lucasbbebber@gmail.com>
2020-03-19 17:52:59 -0300

    Use state instead of ref on hydration hook

--- react/hooks/hydration.tsx
+++ react/hooks/hydration.tsx
@@ -1,24 +1,23 @@
-import { useRef } from 'react'
+import { useState } from 'react'
 
 /** Gets HTML content from elements rendered on the server, before being
  * hydrated on the client (and whether they have been SSRendered at all). */
 export const useDehydratedContent = (hydrationId: string) => {
-  /** On both cases below, the use of useRef is to guarantee that only the values
-   * that it has been initialized with is being considered, to guarantee that it's
+  /** On both cases below, the use of useState is to guarantee that only the values
+   * that it has been initialized with are being considered, to guarantee that it's
    * only taking into account the HTML that's coming from the server, instead of
-   * eventual elements that might be included later.
+   * eventual elements that might be inserted later.
    */
-  const dehydratedElement = useRef(
+  const [dehydratedElement] = useState(() =>
     window?.document?.querySelector?.(`[data-hydration-id="${hydrationId}"]`)
   )
 
-  const hasDehydratedContent = useRef(
-    !!dehydratedElement.current &&
-      dehydratedElement.current.childElementCount > 0
+  const [hasRenderedOnServer] = useState(
+    !!dehydratedElement && dehydratedElement.childElementCount > 0
   )
 
   return {
-    hasRenderedOnServer: hasDehydratedContent.current,
-    dehydratedElement: dehydratedElement.current,
+    hasRenderedOnServer,
+    dehydratedElement,
   }
 }
