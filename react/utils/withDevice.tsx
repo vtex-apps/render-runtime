2186f2dce6a93229c7d6eb69a567b5498789a1e5
Lucas Bebber <lucasbbebber@gmail.com>
2020-05-14 20:07:03 -0300

    Create device detector helper

---
+++ react/utils/withDevice.tsx
@@ -0,0 +1,66 @@
+import React, { FC, ComponentType } from 'react'
+import { useSSR } from '../components/NoSSR'
+import { useMedia } from 'use-media'
+
+export interface Props {
+  runtime: RenderRuntime
+}
+
+export interface WithDeviceProps {
+  deviceInfo: DeviceInfo
+}
+
+export enum Device {
+  phone = 'phone',
+  tablet = 'tablet',
+  desktop = 'desktop',
+  unknown = 'unknown',
+}
+
+export interface DeviceInfo {
+  type: Device
+  isMobile: boolean
+}
+
+const useDevice = (hints: RenderHints) => {
+  const isSSR = useSSR()
+
+  /** These screensizes are hardcoded, based on the default
+   * Tachyons breakpoints. They should probably be the ones
+   * configured via the style.json file, if available. */
+  const isScreenMedium = useMedia({ minWidth: '40rem' })
+  const isScreenLarge = useMedia({ minWidth: '64.1rem' })
+
+  const serverDevice = {
+    type: hints.phone
+      ? Device.phone
+      : hints.tablet
+      ? Device.tablet
+      : Device.desktop,
+    isMobile: hints.mobile,
+  }
+
+  const clientDevice = {
+    type: isScreenLarge
+      ? Device.desktop
+      : isScreenMedium
+      ? Device.tablet
+      : Device.phone,
+    isMobile: !isScreenLarge,
+  }
+
+  return isSSR ? serverDevice : clientDevice
+}
+
+export const withDevice = <P extends Props>(
+  Component: ComponentType<P>
+): FC<P & WithDeviceProps> => {
+  const WithDevice = ({ ...props }: P & WithDeviceProps) => {
+    const hints = props.runtime.hints
+    const deviceInfo = useDevice(hints)
+
+    return <Component deviceInfo={deviceInfo} {...(props as P)} />
+  }
+
+  return WithDevice
+}
