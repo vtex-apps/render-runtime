3a3eaf05fce28bafab449ee1a0f2f132967010cd
Felipe Sales <sales.felipeb@gmail.com>
2020-10-20 11:36:45 -0300

    Fixed GraphQL client link for pages served through Janus

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Fixed
+- GraphQL client link for pages served through Janus.
 
 ## [8.124.3] - 2020-10-30
 ### Fixed
--- react/utils/host.ts
+++ react/utils/host.ts
@@ -1,13 +1,27 @@
 import { canUseDOM } from 'exenv'
 import { RenderRuntime } from '../typings/runtime'
 
-const isRenderServedPage = () => {
-  const generatorMetaTag = document.querySelector(`meta[name='generator']`)
-  const generator = generatorMetaTag && generatorMetaTag.getAttribute('content')
-  return generator && generator.startsWith('vtex.render-server')
+function isIframe(): boolean {
+  return window.location !== window.top.location
 }
 
-export const getBaseURI = (runtime: RenderRuntime) => {
+function getRootDocument(): Document {
+  return isIframe() ? window.top.document : document
+}
+
+function isRenderServedPage(): boolean {
+  const rootDoc = getRootDocument()
+  const generatorMetaTag = rootDoc.querySelector(`meta[name='generator']`)
+  const generator = generatorMetaTag && generatorMetaTag.getAttribute('content')
+
+  const isRender8 = generator && generator.startsWith('vtex.render-server')
+
+  const isServerdThroughJanus = window.location.pathname.startsWith('/api/io')
+
+  return isServerdThroughJanus ? false : !!isRender8
+}
+
+export function getBaseURI(runtime: RenderRuntime): string {
   const { account, workspace, publicEndpoint, rootPath = '' } = runtime
   if (!canUseDOM) {
     return `${workspace}--${account}.${publicEndpoint}`
@@ -15,6 +29,7 @@ export const getBaseURI = (runtime: RenderRuntime) => {
     const {
       location: { hostname },
     } = window
+
     return hostname.endsWith(`.${publicEndpoint}`) || isRenderServedPage()
       ? hostname + rootPath
       : `${hostname}/api/io`
