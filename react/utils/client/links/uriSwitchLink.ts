a69d8da964c93fdbe3a86af8be97e1a84028e64a
Luciano Júnior <luciannojunior@gmail.com>
2020-12-02 16:13:17 -0300

    Switching fallback to scope: private on GraphQL queries

--- react/utils/client/links/uriSwitchLink.ts
+++ react/utils/client/links/uriSwitchLink.ts
@@ -63,7 +63,7 @@ const extractHints = (query: ASTNode, meta: CacheHints) => {
 
   const {
     maxAge = 'long',
-    scope = 'public',
+    scope = 'private',
     version = 1,
     provider,
     sender,
