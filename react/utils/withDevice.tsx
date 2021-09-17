947a71d8b37f1c74ce5b55f808ecd1f152dd0aab
Lucas Bebber <lucasbbebber@gmail.com>
2021-09-17 16:12:16 -0300

    Prevent rerender on withDevice

--- react/utils/withDevice.tsx
+++ react/utils/withDevice.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ComponentType } from 'react'
+import React, { FC, ComponentType, useMemo, useState } from 'react'
 import { useSSR } from '../components/NoSSR'
 import { useMediaLayout } from 'use-media'
 import { RenderRuntime } from '../typings/runtime'
@@ -29,6 +29,7 @@ const useDevice = (hints: RenderRuntime['hints']) => {
   /** These screensizes are hardcoded, based on the default
    * Tachyons breakpoints. They should probably be the ones
    * configured via the style.json file, if available. */
+
   const isScreenMedium = useMediaLayout({ minWidth: '40rem' })
   const isScreenLarge = useMediaLayout({ minWidth: '64.1rem' })
 
@@ -56,11 +57,28 @@ const useDevice = (hints: RenderRuntime['hints']) => {
 export const withDevice = <P extends Props>(
   Component: ComponentType<P & WithDeviceProps>
 ): FC<P> => {
+  const WithDeviceMemo = React.memo(
+    ({ type, isMobile, ...props }: { type: any; isMobile: any }) => (
+      <Component
+        deviceInfo={{ type, isMobile }}
+        {...((props as unknown) as P)}
+      />
+    )
+  )
+
+  WithDeviceMemo.displayName = 'WithDeviceMemo'
+
   const WithDevice = ({ ...props }: P) => {
     const hints = props.runtime.hints
     const deviceInfo = useDevice(hints)
 
-    return <Component deviceInfo={deviceInfo} {...(props as P)} />
+    return (
+      <WithDeviceMemo
+        type={deviceInfo.type}
+        isMobile={deviceInfo.isMobile}
+        {...props}
+      />
+    )
   }
 
   return WithDevice
